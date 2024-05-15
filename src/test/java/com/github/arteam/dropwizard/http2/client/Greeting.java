e97d5b08a76dd9d47ea6128c10dc1427840062f5
Artem Prigoda <arteamon@gmail.com>
2024-05-15 23:37:21 +0200

    Make Greeting a record

--- src/test/java/com/github/arteam/dropwizard/http2/client/Greeting.java
+++ src/test/java/com/github/arteam/dropwizard/http2/client/Greeting.java
@@ -1,20 +1,10 @@
 package com.github.arteam.dropwizard.http2.client;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 /**
  * Date: 1/4/16
  * Time: 5:02 PM
  *
  * @author Artem Prigoda
  */
-public class Greeting {
-
-    public final String author;
-    public final String message;
-
-    public Greeting(String author, String message) {
-        this.author = author;
-        this.message = message;
-    }
+public record Greeting(String author, String message) {
 }
